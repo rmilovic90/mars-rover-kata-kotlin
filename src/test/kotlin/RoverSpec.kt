ff1d364aa2529b88eb6926a5c1cb93fc56664864
Rade Milovic <r.milovic90@gmail.com>
2018-02-27 23:37:24 +0100

    We are now able to rotate rover right how many times we want from its starting position.

--- src/test/kotlin/RoverSpec.kt
+++ src/test/kotlin/RoverSpec.kt
@@ -5,10 +5,17 @@ import org.jetbrains.spek.api.dsl.it
 
 object RoverSpec : Spek({
 	describe("a rover") {
-		val rover = Rover()
+		lateinit var rover: Rover
+
+		beforeEachTest {
+			rover = Rover()
+		}
 
 		listOf(
-			listOf("R", "0:0:E")
+			listOf("R", "0:0:E"),
+			listOf("RR", "0:0:S"),
+			listOf("RRR", "0:0:W"),
+			listOf("RRRR", "0:0:N")
 		).forEach { test ->
 			it("should rotate right (comands: ${test[0]}, expected position: ${test[1]})") {
 				rover.execute(test[0]) shouldEqual test[1]
@@ -18,5 +25,20 @@ object RoverSpec : Spek({
 })
 
 class Rover {
-	fun execute(commands: String) = "0:0:E"
+	private var direction = "N"
+
+	fun execute(commands: String): String {
+		commands.forEach {
+			direction = rotateRight()
+		}
+
+		return "0:0:$direction"
+	}
+
+	private fun rotateRight() = when (direction) {
+		"N" -> "E"
+		"E" -> "S"
+		"S" -> "W"
+		else -> "N"
+	}
 }
