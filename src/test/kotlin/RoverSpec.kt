b2e4f1fdccf26c5d03ab53c671e2839cbba956b6
Rade Milovic <r.milovic90@gmail.com>
2018-02-28 00:39:22 +0100

    We have removed duplicated logic for rotating in the Direction enum.

--- src/test/kotlin/RoverSpec.kt
+++ src/test/kotlin/RoverSpec.kt
@@ -63,11 +63,11 @@ enum class Direction(private val value: String, private val left: String, privat
 
 	fun value() = value
 
-	fun left() = values().firstOrNull { direction ->
-		direction.value == this.left
-	}
+	fun left() = directionToThe(left)
 
-	fun right() = values().firstOrNull { direction ->
-		direction.value == this.right
+	fun right() = directionToThe(right)
+
+	private fun directionToThe(direction: String) = values().firstOrNull { currentDirection ->
+		currentDirection.value == direction
 	}
 }
